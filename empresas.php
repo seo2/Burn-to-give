667d640711ab0b6003756b47328ea2bb46d96ddc
fchristiansen <felipe@seo2.cl>
2018-03-06 20:36:23 -0300

    primer html page empresas
    -pendiente ubicacion de imagenes
    -revision mobile
    -ajuste de tamaños

---
+++ empresas.php
@@ -0,0 +1,195 @@
+<?php include('header.php'); ?>
+
+	<?php // include('main-nav.php'); ?>
+	<header class="fondo-degradado-naranja position-relative">
+		<div class="container position-relative">
+			<div class="selector-idioma">
+				<ul>
+					<li>
+						<a class="selected" href="">esp</a>
+					</li>
+					<li class="divisor">|</li>
+					<li>
+						<a href="">eng</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<img class="img-responsive logo-ingresa" src="assets/img/logo-ingresa.png" alt="">
+		<img class="img-responsive texto-ingresa center" src="assets/img/texto-ingresa.png" alt="">
+
+	</header>
+	<section class="video">
+		<div class="container-fluid">
+				<video src=""></video>
+		</div>
+	</section>
+	<section class="que-es">
+		<div class="container">
+			<div class="row">
+				<div class="bloque-amarillo">
+					<h2>¿que es burn to give?</h2>
+					<p>Burn to Give es una plataforma que
+						recicla las calorías quemadas haciendo cualquier tipo de actividad
+					física y las convierte en nutrición de urgencia para un niño con
+					 desnutrición que las necesita.</p>
+				</div>
+				<div class="bloque-celeste borde-amarillo ">
+					<p>Por cada caloría quemada haciendo
+								deportes, una caloría es entregada a un niño sufriendo de desnutrición.</p>
+
+					<p>Burn to Give utiliza la tecnología para
+								conectar dos pandemias globales como son la obesidad y desnutrición,
+								de una manera que ayude a solucionar ambas.</p>
+
+					<p>Queremos motivar a millones de personas a llevar una vida sana y
+								activa, inspiradas por el deseo de ayudar a quienes más lo necesitan</p>
+
+					<p>En Burn to Give queremos formar el equipo más grande de personas
+								 que queman calorías para donarlas como alimento a las personas
+								 que más lo necesitan y queremos que seas parte de esta increíble causa.</p>
+					 <p class="hash text-right">#BurnToGive</p>
+
+				</div>
+			</div>
+		</div>
+
+	</section>
+	<section class="como">
+		<div class="container">
+			<div class="row">
+				<h2 class="texto-amarillo-italic">¿CÓMO  <br> PUEDES AYUDAR?</h2>
+				<div class="caja-texto-sombra  pb-100 mb-100">
+					<p><span class="texto-azul">A partir del 15 de marzo</span><br>
+					<span class="texto-amarillo">y hasta 15 de abril</span></p>
+						<ul>
+							<li>Haciendo deporte o algún tipo de actividad física.</li>
+							<li>Entra a <a class="texto-naranjo" href="http://www.burntogive.com" target="_blank">www.burntogive.com</a>e ingresa tus calorías quemadas.</li>
+							<li>Comparte en redes sociales tus fotos haciendo deporte mencionando a @burntogive y utilizando
+							#BurnToGive, para motivar a tus familiares, amigos, colegas y cercanos a hacer lo mismo</li>
+						</ul>
+						<h3 class="llamado">¡Necesitamos tu ayuda!</h3>
+						<p>Este 15 de marzo comenzamos nuestro PRIMER desafío. Durante 30 días, vamos a motivar a miles de personas a hacer deporte para ayudar a otros. Sé parte de esta increíble iniciativa y ayúdanos a cumplir nuestra meta de 30 millones de calorías, con las que vamos a alimentar y salvar la vida de más de 400 niños sufriendo de desnutrición en Haití. Ellos te lo van a agradecer, y tu cuerpo también.</p>
+						<div class="pie-de-caja huincha">
+							<p class="hash">#BurnToGive</p>
+						</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<section class="historia">
+		<div class="container">
+			<div class="row">
+				<h2>historia</h2>
+				<div class="caja-texto-sombra mb-30 pb-100">
+					<p class="texto-azul">
+						La Historia de Burn to Give (contada por nuestro fundador, Eduardo della Maggiora)
+					</p>
+					<p>El año 2014 dejé Nueva York para irme como voluntario a África del Este. Por años me había
+						ganado la vida trabajando en una de las instituciones financieras más grandes del mundo.
+						Tenía un trabajo increíble, muchos amigos y vivía en una de las ciudades más fascinantes del mundo.
+						Me sentía muy privilegiado, pero a pesar de todo esto, de alguna manera, sentía que algo faltaba.
+						No estaba seguro que es lo que esto era, pero mi “guata” me decía que tenía que experimentar
+						algo completamente distinto y opuesto
+						a lo que estaba haciendo hasta ese
+						momento. Elegí Tanzania como mi destino.</p>
+						<div class="pie-de-caja bloque-gris">
+
+						</div>
+				</div>
+				<div class="caja-texto-sombra mb-30 pb-100">
+					<div class="linea-azul-xs clearfix"> </div>
+					<p>Me inscribí en un programa de voluntariado, en el cual hacía clases de inglés y matemáticas a niños
+					en un colegio de Moshi, un pueblo muy pequeño al lado de Kilimanjaro. Todos los días después de
+					 terminar las clases, les dábamos una especie de almuerzo a los niños en base a maíz y agua.
+					Algo que me llamó mucho la atención fue qué para más de la mitad de mis alumnos, esta era el
+					 único alimento que comían en todo el día. Fue así cómo me tocó ver y conocer de cerca la devastadora
+					realidad de la desnutrición infantil.</p>
+					<div class="pie-de-caja bloque-gris">
+
+						</div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+	<section class="problema">
+		<div class="container">
+			<div class="row">
+				<h2 class="titulo-azul">el problema</h2>
+				<h3>Obesidad</h3>
+				<div class="caja-texto-sombra mb-30 pb-100"">
+					<h4>En el mundo</h4>
+					<ul>
+						<li>La obesidad en el mundo se ha tri-plicado desde 1975</li>
+						<li>Cerca de un tercio de la población mundial (más de 2 billones de personas) tiene sobrepeso u obesidad</li>
+						<li>El 65% de la población mundial vive en países donde el sobrepeso y obesidad mata a más personas que la desnutrición</li>
+					</ul>
+					<div class="pie-de-caja bloque-gris">
+
+					</div>
+				</div>
+				<div class="caja-texto-sombra mb-30 pb-100"">
+					<h4>En Chile</h4>
+					<ul>
+					<li>Chile ocupa el #1 lugar en obesidad infantil en Latinoamérica, y el #6 a nivel mundial</li>
+					<li>35% de niños menores de 6 años presentan sobrepeso u obesidad,
+					duplicando las cifras de hace 10 años</li>
+					<li>Tres de cada cuatro Chilenos están excedidos de peso y el 87% tiene hábitos sedentarios
+						(definido como no hacer deporte en los últimos 30 días)</li>
+					</ul>
+
+					<div class="pie-de-caja bloque-gris"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="empresas bloque-celeste">
+		<div class="container">
+			<div class="row">
+				<div class="">
+					<h2>Empresas</h2>
+						<p>Para convertir las calorías quemadas en nutrición de urgencia, se necesitan
+						empresas visionarias e innovadoras que patrocinen estas calorías que las personas
+						están donando. Al hacerlo están generando un doble impacto en la sociedad,
+						por una parte están motivando a miles de personas a llevar una vida sana y
+						activa y por otro lado están contribuyendo a alimentar a miles de niños alrededor del mundo. </p>
+						<p class="texto-amarillo"> ¡No podríamos hacer esto  <br> sin su apoyo!</p>
+
+						<img class="img-responsive center-block grilla-logos" src="assets/img/logos-footer.png" alt="">
+
+				</div>
+			</div>
+		</div>
+
+	</section>
+	<section class="preguntas">
+		<div class="container">
+			<div class="row">
+				<h2>preguntas <br> frecuentes</h2>
+				<div class="caja-texto-sombra">
+					<ul>
+						<li>¿Qué significa RUTF?</li>
+						<li>¿Cuánto dura un tratamiento en base a RUTF?</li>
+						<li>¿Cuantas calorías tiene un paquete de RUTF?</li>
+						<li>¿Como se financian las calorías que estamos quemando haciendo deporte?</li>
+						<li>¿Hay un máximo de calorías que puedo quemar y regalar?</li>
+						<li>¿Cómo se entrega la ayuda en Haití?</li>
+						<li>¿Con que fundaciones se está trabajando?</li>
+						<li>¿Por qué entregar la nutrición de urgencia en Haití y no en Chile?</li>
+						<li>¿Qué significa que esta sea la versión Beta de Burn to Give?</li>
+						<li>¿Qué tipo de empresa es Burn to Give?</li>
+						<li>¿Cuál es la diferencia entre una empresa social y</li>
+						<li>una sin tfines de lucro?</li>
+						<li>¿A partir de cuándo puedo ayudar con burn tu give?</li>
+					</ul>
+					<div class="pie-de-caja bloque-gris"></div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+
+<?php include('footer.php'); ?>
+
