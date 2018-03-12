2324b5139bca39749fd7223dc180e2b1740a33ef
fchristiansen <felipe@seo2.cl>
2018-03-12 12:51:01 -0300

    ajuste empresas

--- empresas.php
+++ empresas.php
@@ -29,6 +29,7 @@
 			<div class="row">
 				<div class="bloque-amarillo">
 					<h2>¿que es burn to give?</h2>
+					<div class="clear"></div>
 					<p>Burn to Give es una plataforma que
 						recicla las calorías quemadas haciendo cualquier tipo de actividad
 					física y las convierte en nutrición de urgencia para un niño con
@@ -36,18 +37,18 @@
 				</div>
 				<div class="bloque-celeste borde-amarillo ">
 					<p>Por cada caloría quemada haciendo
-								deportes, una caloría es entregada a un niño sufriendo de desnutrición.</p>
+						deportes, una caloría es entregada a un niño sufriendo de desnutrición.</p>
 
 					<p>Burn to Give utiliza la tecnología para
-								conectar dos pandemias globales como son la obesidad y desnutrición,
-								de una manera que ayude a solucionar ambas.</p>
+						conectar dos pandemias globales como son la obesidad y desnutrición,
+						de una manera que ayude a solucionar ambas.</p>
 
 					<p>Queremos motivar a millones de personas a llevar una vida sana y
-								activa, inspiradas por el deseo de ayudar a quienes más lo necesitan</p>
+						activa, inspiradas por el deseo de ayudar a quienes más lo necesitan</p>
 
-					<p>En Burn to Give queremos formar el equipo más grande de personas
-								 que queman calorías para donarlas como alimento a las personas
-								 que más lo necesitan y queremos que seas parte de esta increíble causa.</p>
+					<p class="mb-0">En Burn to Give queremos formar el equipo más grande de personas
+						que queman calorías para donarlas como alimento a las personas
+						que más lo necesitan y queremos que seas parte de esta increíble causa.</p>
 					 <p class="hash text-right">#BurnToGive</p>
 
 				</div>
@@ -59,18 +60,32 @@
 		<div class="container position-relative mw">
 			<img class="img-responsive bg-como" src="assets/img/bg-como.jpg" alt="">
 			<div class="row">
-				<h2 class="texto-amarillo-italic">¿CÓMO  <br> PUEDES AYUDAR?</h2>
-				<div class="caja-texto-sombra  pb-100 mb-100">
-					<p><span class="texto-azul">A partir del 15 de marzo</span><br>
-					<span class="texto-amarillo">y hasta 15 de abril</span></p>
-						<ul>
-							<li>Haciendo deporte o algún tipo de actividad física.</li>
-							<li>Entra a <a class="texto-naranjo" href="http://www.burntogive.com" target="_blank">www.burntogive.com</a>e ingresa tus calorías quemadas.</li>
-							<li>Comparte en redes sociales tus fotos haciendo deporte mencionando a @burntogive y utilizando
-							#BurnToGive, para motivar a tus familiares, amigos, colegas y cercanos a hacer lo mismo</li>
-						</ul>
+				<h2 class="texto-amarillo-italic">¿CÓMO  PUEDES  <br> AYUDAR?</h2>
+
+				<div class="caja-texto-sombra  pb-100">
+						<div class="padding-30">
+						<p>	<span class="texto-azul">A partir del 15 de marzo</span>
+						<span class="texto-amarillo">y hasta 15 de abril</span></p>
+
+							<ul>
+								<li>Haciendo deporte o algún tipo de actividad física.</li>
+								<li>Entra a <a class="texto-naranjo" href="http://www.burntogive.com" target="_blank">www.burntogive.com</a> e ingresa tus calorías quemadas.</li>
+								<li>Comparte en redes sociales tus fotos haciendo deporte mencionando a @burntogive y utilizando
+								#BurnToGive, para motivar a tus familiares, amigos, colegas y cercanos a hacer lo mismo</li>
+							</ul>
+						</div>
+
 						<h3 class="llamado">¡Necesitamos tu ayuda!</h3>
-						<p>Este 15 de marzo comenzamos nuestro PRIMER desafío. Durante 30 días, vamos a motivar a miles de personas a hacer deporte para ayudar a otros. Sé parte de esta increíble iniciativa y ayúdanos a cumplir nuestra meta de 30 millones de calorías, con las que vamos a alimentar y salvar la vida de más de 400 niños sufriendo de desnutrición en Haití. Ellos te lo van a agradecer, y tu cuerpo también.</p>
+						<div class="padding-30 pt-0 pb-0">
+								<p>Este 15 de marzo comenzamos nuestro PRIMER desafío. Durante 30 días,
+								vamos a motivar a miles de personas a hacer deporte para ayudar a otros.
+								 Sé parte de esta increíble iniciativa y ayúdanos a cumplir nuestra meta
+								de 30 millones de calorías, con las que vamos a alimentar y salvar la vida
+								 de más de 400 niños sufriendo de desnutrición en Haití. Ellos te lo van a
+								agradecer, y tu cuerpo también.</p>
+								</div>
+
+
 						<div class="pie-de-caja huincha">
 							<p class="hash">#BurnToGive</p>
 						</div>
@@ -84,7 +99,7 @@
 			<img class="img-responsive bg-historia" src="assets/img/bg-historia.png" alt="">
 			<div class="row">
 				<h2>historia</h2>
-				<div class="caja-texto-sombra mb-30 pb-100">
+				<div class="caja-texto-sombra mb-30">
 					<p class="texto-azul">
 						La Historia de Burn to Give (contada por nuestro fundador, Eduardo della Maggiora)
 					</p>
@@ -100,17 +115,16 @@
 
 						</div>
 				</div>
-				<div class="caja-texto-sombra mb-30 pb-100">
+				<div class="caja-texto-sombra mb-30">
 					<div class="linea-azul-xs clearfix"> </div>
-					<p>Me inscribí en un programa de voluntariado, en el cual hacía clases de inglés y matemáticas a niños
-					en un colegio de Moshi, un pueblo muy pequeño al lado de Kilimanjaro. Todos los días después de
-					 terminar las clases, les dábamos una especie de almuerzo a los niños en base a maíz y agua.
-					Algo que me llamó mucho la atención fue qué para más de la mitad de mis alumnos, esta era el
-					 único alimento que comían en todo el día. Fue así cómo me tocó ver y conocer de cerca la devastadora
-					realidad de la desnutrición infantil.</p>
-					<div class="pie-de-caja bloque-gris">
 
-						</div>
+							<p>Me inscribí en un programa de voluntariado, en el cual hacía clases de inglés y matemáticas a niños
+							en un colegio de Moshi, un pueblo muy pequeño al lado de Kilimanjaro. Todos los días después de
+							 terminar las clases, les dábamos una especie de almuerzo a los niños en base a maíz y agua.
+							Algo que me llamó mucho la atención fue qué para más de la mitad de mis alumnos, esta era el
+							 único alimento que comían en todo el día. Fue así cómo me tocó ver y conocer de cerca la devastadora
+							realidad de la desnutrición infantil.</p>
+						<div class="pie-de-caja bloque-gris"></div>
 				</div>
 			</div>
 		</div>
@@ -118,36 +132,42 @@
 	</section>
 	<section class="problema">
 		<div class="container mw">
-			<div class="row">
-				<h2 class="titulo-azul">el problema</h2>
-				<h3>Obesidad</h3>
-				<div class="caja-texto-sombra mb-30 pb-100"">
-					<h4>En el mundo</h4>
-					<ul>
-						<li>La obesidad en el mundo se ha tri-plicado desde 1975</li>
-						<li>Cerca de un tercio de la población mundial (más de 2 billones de personas) tiene sobrepeso u obesidad</li>
-						<li>El 65% de la población mundial vive en países donde el sobrepeso y obesidad mata a más personas que la desnutrición</li>
-					</ul>
-					<div class="pie-de-caja bloque-gris">
+			<h2 class="titulo-azul">el problema</h2>
 
-					</div>
-				</div>
-				<div class="caja-texto-sombra mb-30 pb-100"">
+			<div class="row position-relative">
+				<h3>Obesidad</h3>
+			<img class="img-responsive bg-obesidad" src="assets/img/bg-obesidad.jpg" alt="">
+
+
+				<div class="caja-texto-sombra mb-30">
+
+						<h4>En el mundo</h4>
+						<ul>
+							<li>La obesidad en el mundo se ha tri-plicado desde 1975</li>
+							<li>Cerca de un tercio de la población mundial (más de 2 billones de personas) tiene sobrepeso u obesidad</li>
+							<li>El 65% de la población mundial vive en países donde el sobrepeso y obesidad mata a más personas que la desnutrición</li>
+						</ul>
+
+
+					<div class="pie-de-caja bloque-gris"></div>
+				</div><!-- caja texto sombra -->
+				<div class="caja-texto-sombra mb-30">
 					<h4>En Chile</h4>
 					<ul>
-					<li>Chile ocupa el #1 lugar en obesidad infantil en Latinoamérica, y el #6 a nivel mundial</li>
-					<li>35% de niños menores de 6 años presentan sobrepeso u obesidad,
-					duplicando las cifras de hace 10 años</li>
-					<li>Tres de cada cuatro Chilenos están excedidos de peso y el 87% tiene hábitos sedentarios
-						(definido como no hacer deporte en los últimos 30 días)</li>
+						<li>Chile ocupa el #1 lugar en obesidad infantil en Latinoamérica, y el #6 a nivel mundial</li>
+						<li>35% de niños menores de 6 años presentan sobrepeso u obesidad,
+						duplicando las cifras de hace 10 años</li>
+						<li>Tres de cada cuatro Chilenos están excedidos de peso y el 87% tiene hábitos sedentarios
+							(definido como no hacer deporte en los últimos 30 días)</li>
 					</ul>
 
 					<div class="pie-de-caja bloque-gris"></div>
-				</div>
-			</div>
+				</div><!-- caja texto sombra -->
+
+		</div>
 		</div>
 	</section>
-	<section class="empresas bloque-celeste">
+	<section class="empresas fondo-azul-2">
 		<div class="container mw">
 			<div class="row">
 				<div class="">
@@ -157,21 +177,65 @@
 						están donando. Al hacerlo están generando un doble impacto en la sociedad,
 						por una parte están motivando a miles de personas a llevar una vida sana y
 						activa y por otro lado están contribuyendo a alimentar a miles de niños alrededor del mundo. </p>
-						<p class="texto-amarillo"> ¡No podríamos hacer esto  <br> sin su apoyo!</p>
+						<p class="texto-amarillo"> ¡No podríamos hacer  <br> esto  sin su apoyo!</p>
 
 						<img class="img-responsive center-block grilla-logos" src="assets/img/logos-footer.png" alt="">
 
 				</div>
 			</div>
 		</div>
-
-	</section>
+	</section> <!-- empresas -->
 	<section class="preguntas">
 		<div class="container mw">
+			<img class="img-responsive bg-faq" src="assets/img/bg-faq.jpg" alt="">
 			<div class="row">
 				<h2>preguntas <br> frecuentes</h2>
 				<div class="caja-texto-sombra">
-					<ul>
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingOne">
+      <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible Group Item #1
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingTwo">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Collapsible Group Item #2
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="headingThree">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Collapsible Group Item #3
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+			<!-- 		<ul>
 						<li>¿Qué significa RUTF?</li>
 						<li>¿Cuánto dura un tratamiento en base a RUTF?</li>
 						<li>¿Cuantas calorías tiene un paquete de RUTF?</li>
@@ -185,13 +249,11 @@
 						<li>¿Cuál es la diferencia entre una empresa social y</li>
 						<li>una sin tfines de lucro?</li>
 						<li>¿A partir de cuándo puedo ayudar con burn tu give?</li>
-					</ul>
+					</ul> -->
 					<div class="pie-de-caja bloque-gris"></div>
-				</div>
+				</div> <!-- caja texto sombra -->
 			</div>
 		</div>
-
-	</section>
-
+	</section> <!-- preguntas -->
 <?php include('footer.php'); ?>
 
