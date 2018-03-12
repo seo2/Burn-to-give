efaec0d057afe28556e4e82120a4598d77994c28
fchristiansen <felipe@seo2.cl>
2018-03-12 15:49:47 -0300

    css empresas

---
+++ faq-esp.php
@@ -0,0 +1,171 @@
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+						<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q1" aria-expanded="true" aria-controls="q1">
+										¿Qué significa RUTF?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam numquam distinctio tempore accusamus eligendi.</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q2" aria-expanded="true" aria-controls="q2">
+										¿Cuánto dura un tratamiento en base a RUTF?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q3" aria-expanded="true" aria-controls="q3">
+										¿Cuántas calorías tiene un paquete de RUTF?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q4" aria-expanded="true" aria-controls="q4">
+										¿Cómo se financian las calorías que estamos quemando haciendo deporte?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q5" aria-expanded="true" aria-controls="q5">
+										¿Hay un máximo de calorías que puedo quemar y regalar?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q5" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q6" aria-expanded="true" aria-controls="q6">
+										¿Cómo se entrega la ayuda en Haití?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q7" aria-expanded="true" aria-controls="q7">
+										¿Con que fundaciones se está trabajando?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q7" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q8" aria-expanded="true" aria-controls="q8">
+										¿Por qué entregar la nutrición de urgencia en Haití y no en Chile?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q8" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q9" aria-expanded="true" aria-controls="q9">
+										¿Qué significa que esta sea la versión Beta de Burn to Give?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q9" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q10" aria-expanded="true" aria-controls="q10">
+										¿Qué tipo de empresa es Burn to Give?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q10" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q11" aria-expanded="true" aria-controls="q11">
+										¿Cuál es la diferencia entre una empresa social y una sin tfines de lucro?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q11" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+                    	<div class="panel panel-default">
+                        	<div class="panel-heading" role="tab" id="">
+								<h4 class="panel-title">
+									<a class="cd-faq-trigger" role="button" data-toggle="collapse" data-parent="#accordion" href="#q12" aria-expanded="true" aria-controls="q12">
+										¿A partir de cuándo puedo ayudar con burn tu give?
+									</a>
+                        		</h4>
+                        	</div>
+							<div id="q12" class="panel-collapse collapse" role="tabpanel" aria-labelledby="" aria-expanded="true" style="">
+								<div class="panel-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, modi?</p>
+                        		</div>
+                        	</div>
+                    	</div><!-- panel -->
+
+				</div> <!-- panel faq -->
