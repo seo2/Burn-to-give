ebf75492bcd48ce30cce945e156e1611dc0ab5db
Carolina Rojas <carolina@seo2.cl>
2018-03-13 11:53:14 -0300

    upload

--- ajax/upload.php
+++ ajax/upload.php
@@ -9,6 +9,31 @@ $db = new MysqliDb (HOST, USERNAME, PASSWORD, DATABASE);
 $userID = $_POST["userID"];
 $op = $_POST["op"];
 
+function image_fix_orientation($filename)
+    {
+        $exif = exif_read_data($filename);
+        if (!empty($exif['Orientation']))
+        {
+            
+            switch ($exif['Orientation'])
+            {
+                case 3:
+                    return(180);
+                    break;
+
+                case 6:
+                    return(90);
+                    break;
+
+                case 8:
+                    return(-90);
+                    break;
+            }
+
+            
+        }
+    }
+
 //sacamos los datos de las calorias
         $db->where ("calID", $userID);
 		$user = $db->getOne ("calorias");
@@ -18,15 +43,16 @@ $op = $_POST["op"];
 			$calorias = 0;
 		}
 
-$target_dir 	= "../uploads/fotos/";
-$target_dir2 	= "../uploads/";
-$fecha 			= date("YmdHis");
+$target_dir = "../uploads/fotos/";
+$target_dir2 = "../uploads/";
+$fecha = date("YmdHis");
+$abs_path = dirname( __FILE__ );
 
 if($op == 'personalizada'){
-	$partes_ruta 	= pathinfo($target_dir . basename($_FILES["fileToUpload"]["name"]));
-	$imageFileType 	= strtolower($partes_ruta['extension']);
-	$filename 		= $fecha.".".$imageFileType;
-	$target_file 	= $target_dir . $filename;
+$partes_ruta = pathinfo($target_dir . basename($_FILES["fileToUpload"]["name"]));
+$imageFileType = strtolower($partes_ruta['extension']);
+$filename = $fecha.".".$imageFileType;
+$target_file = $target_dir . $filename;
 }else{
 
 }
@@ -36,84 +62,99 @@ $update = false;
 
 // Check if image file is a actual image or fake image
 if($op == 'personalizada'){
-	if(isset($_POST["submit"])) {
-	    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-	    if($check !== false) {
-	        //echo "File is an image - " . $check["mime"] . ".";
-	        $uploadOk = 1;
-	    } else {
-	        //echo "File is not an image.";
-	        $uploadOk = 0;
-	    }
-	}
-	// Check if file already exists
-	if (file_exists($target_file)) {
-	    //echo "Sorry, file already exists.";
-	    $uploadOk = 0;
-	}
-	// Check file size
-	if ($_FILES["fileToUpload"]["size"] > 9999999) {
-	    //echo "Sorry, your file is too large.";
-	    $uploadOk = 0;
-	}
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        //echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        //echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    //echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 9999999) {
+    //echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
 
 
-	// Allow certain file formats
-	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-	    //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-	    $uploadOk = 0;
-	}
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-	    //echo "Sorry, your file was not uploaded.";
-		// if everything is ok, try to upload file
-	} else {
-	    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-	        //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-	        //HACER RESIZE
-	        
-	        $exif = exif_read_data($target_dir.$filename);
-			$ort = $exif['Orientation'];
-	        
-	        $image 		= WideImage::load($target_dir.$filename);
-	
-	        $image_w 	= $image->getWidth();
-	        $image_h 	= $image->getHeight();
-	
-	        $image 		= $image->resize(1600, 1200);
-	
-	        $image->crop('center', 'center', 800, 800)->resize(600, 600)->saveToFile($target_dir.$filename);
-	
-			$abs_path = dirname( __FILE__ );
-	
-			$last_filename 	=  $target_dir.$filename;
-			$mask 			= "../assets/img/mascara.png";
-			$PngFile 		= $fecha.".png";
-			$newfile_name 	= $target_dir2.$PngFile;
-	
-			$img 			= WideImage::load($last_filename);
-			$watermark 		= WideImage::load($mask);
-			$new 			= $img->merge($watermark, 'center', 'bottom', 100);
-	
-			$canvas = $new->getCanvas();
-			$canvas->useFont($abs_path.'/fonts/Gotham-Black.ttf', 33, $new->allocateColor(235, 83, 31));
-	
-			if($calorias > 999){
-				$canvas->writeText('right - 22', 'bottom - 62', $calorias);
-			}else{
-				$canvas->writeText('right - 42', 'bottom - 62', $calorias);
-			}
-	
-			//guardar en el server la foto png con el marco
-			$new->saveToFile($newfile_name);
-	
-	        $update = true;
-	
-	    } else {
-	        //echo "Sorry, there was an error uploading your file.";
-	    	$uploadOk = 0;
-	    }
-	}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    //echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    	//CHEQUEAR ORIENTACION
+    	$grados = image_fix_orientation($target_file);
+    	if($grados!= ""){
+    		WideImage::load($target_file)->rotate($grados)->saveToFile($target_file);
+    	}
+
+        //HACER RESIZE
+        $image = WideImage::load($target_dir.$filename);
+
+        $image_w = $image->getWidth();
+        $image_h = $image->getHeight();
+
+        if($image_h > $image_w){
+        	$image = $image->resize(630, '100%');
+        }elseif($image_w > $image_h){
+        	$image = $image->resize('100%', 630);
+        }else{
+        	$image = $image->resize(630, 630);
+        }
+
+        $image->crop('center', 'center', 630, 630)->saveToFile($target_dir.$filename);
+
+        
+
+		
+
+		$last_filename =  $target_dir.$filename;
+		$mask = "../assets/img/mascara.png";
+		$PngFile = $fecha.".png";
+		$newfile_name = $target_dir2.$PngFile;
+
+		$img = WideImage::load($last_filename);
+		$watermark = WideImage::load($mask);
+		$new = $img->merge($watermark, 'center', 'bottom', 100);
+
+		$canvas = $new->getCanvas();
+		$canvas->useFont($abs_path.'/fonts/Gotham-Black.ttf', 36, $new->allocateColor(235, 83, 31));
+
+		if($calorias > 999){
+			$canvas->writeText('right - 26', 'bottom - 68', $calorias);
+		}elseif($calorias < 100 && $calorias > 9){
+			$canvas->writeText('right - 56', 'bottom - 68', $calorias);
+		}elseif($calorias < 10){
+			$canvas->writeText('right - 72', 'bottom - 68', $calorias);
+		}else{
+			$canvas->writeText('right - 42', 'bottom - 68', $calorias);
+		}
+
+		//guardar en el server la foto png con el marco
+		$new->saveToFile($newfile_name);
+
+        $update = true;
+
+    } else {
+        //echo "Sorry, there was an error uploading your file.";
+    	$uploadOk = 0;
+    }
+}
 }else{
 
 		$PngFile = $fecha.".png";
@@ -124,12 +165,16 @@ if($op == 'personalizada'){
 		
 
 		$canvas = $img->getCanvas();
-		$canvas->useFont($abs_path.'/fonts/Gotham-Black.ttf', 40, $img->allocateColor(255, 255, 255));
+		$canvas->useFont($abs_path.'/fonts/Gotham-Black.ttf', 54, $img->allocateColor(255, 255, 255));
 
 		if($calorias > 999){
-			$canvas->writeText('right - 80', 'bottom - 70', $calorias);
+			$canvas->writeText('right - 50', 'bottom - 70', $calorias);
+		}elseif($calorias < 100 && $calorias > 9){
+			$canvas->writeText('right - 86', 'bottom - 68', $calorias);
+		}elseif($calorias < 10){
+			$canvas->writeText('right - 108', 'bottom - 68', $calorias);
 		}else{
-			$canvas->writeText('right - 100', 'bottom - 70', $calorias);
+			$canvas->writeText('right - 62', 'bottom - 70', $calorias);
 		}
 
 		//guardar en el server la foto png con el marco
